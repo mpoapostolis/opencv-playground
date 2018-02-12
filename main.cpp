e3725b5d7d8bdbfa0830e05619749c2b2666306e
mpoapostolis <mpoapostolis@gmail.com>
2018-02-13 01:35:05 +0200

    draw multiple rects on lena

--- main.cpp
+++ main.cpp
@@ -10,43 +10,48 @@ void DrawRectangle(Mat& img, Rect box);
 
 Rect g_rectangle;
 bool g_bDrawingBox = false;
-
+vector<Rect> rects;
 int main(int argc, char** argv) {
-	Mat img = imread(argv[1]);
-	namedWindow(WINDOW_NAME, CV_WINDOW_AUTOSIZE);
-  setMouseCallback(WINDOW_NAME, on_MouseHandle, (void*) &img);
+  Mat img = imread(argv[1]);
+  namedWindow(WINDOW_NAME, CV_WINDOW_AUTOSIZE);
+  setMouseCallback(WINDOW_NAME, on_MouseHandle, (void*)&img);
   imshow(WINDOW_NAME, img);
   waitKey();
   return 0;
 }
 
 void on_MouseHandle(int event, int x, int y, int flags, void* param) {
-	Mat& image = *(Mat*) param;
+  Mat& image = *(Mat*)param;
   switch (event) {
-  	case EVENT_MOUSEMOVE: {    // When mouse moves, get the current rectangle's width and height
-  		if (g_bDrawingBox) {
-  			g_rectangle.width = x - g_rectangle.x;
-  			g_rectangle.height = y - g_rectangle.y;
+    case EVENT_MOUSEMOVE: {
+      if (g_bDrawingBox) {
+        g_rectangle.width = x - g_rectangle.x;
+        g_rectangle.height = y - g_rectangle.y;
         DrawRectangle(image, g_rectangle);
-  		}
+      }
       break;
-  	}
-  	case EVENT_LBUTTONDOWN: {  // when the left mouse button is pressed down,
+    }
+    case EVENT_LBUTTONDOWN: {
       g_bDrawingBox = true;
       g_rectangle = Rect(x, y, 0, 0);
       break;
-  	}
-  	case EVENT_LBUTTONUP: {   //when the left mouse button is released,
-  		g_bDrawingBox = false;
+    }
+    case EVENT_LBUTTONUP: {
+      g_bDrawingBox = false;
+      rects.push_back(g_rectangle);
       break;
-  	}
-	}
+    }
+  }
 }
 
-void DrawRectangle(Mat& img, Rect box){
-  Mat zeros = Mat::zeros(img.size(), img.type());
-	rectangle(zeros, box.tl(), box.br(), Scalar(0,255,0));
+void DrawRectangle(Mat& img, Rect box) {
+  Mat old_rects = Mat::zeros(img.size(), img.type());
+  Mat current_rect = Mat::zeros(img.size(), img.type());
+  for (size_t i = 0; i < rects.size(); i++) {
+    rectangle(old_rects, rects[i].tl(), rects[i].br(), Scalar(0, 255, 0));
+  };
   Mat res;
-  add(img, zeros, res);
+  rectangle(current_rect, box.tl(), box.br(), Scalar(0, 255, 0));
+  res = old_rects + current_rect + img;
   imshow(WINDOW_NAME, res);
 }
